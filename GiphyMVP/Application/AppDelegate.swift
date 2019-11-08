fa44d7467f608375f2c26c14a03f7a2d6ae8e1bd
Alexander Serdobintsev <aserdobintsev@gmail.com>
2019-11-08 07:21:07 +0500

    Add navigation controller for better looking

--- GiphyMVP/Application/AppDelegate.swift
+++ GiphyMVP/Application/AppDelegate.swift
@@ -16,7 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let window = UIWindow()
-        window.rootViewController = configure()
+
+        let navigationController = UINavigationController(rootViewController: configure())
+        window.rootViewController = navigationController
         window.makeKeyAndVisible()
         self.window = window
         return true
