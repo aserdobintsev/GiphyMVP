7c7da009f29a01ade18e1f3efc299a5abff50109
Alexander Serdobintsev <aserdobintsev@gmail.com>
2019-11-22 15:31:36 +0500

    Add GiphyAPI module generated from OpenAPI specification

---
+++ GiphyAPI/GIphy.h
@@ -0,0 +1,19 @@
+//
+//  GIphy.h
+//  GIphy
+//
+//  Created by Alexander Serdobintsev on 11/22/19.
+//  Copyright © 2019 Alexander Serdobintsev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GIphy.
+FOUNDATION_EXPORT double GIphyVersionNumber;
+
+//! Project version string for GIphy.
+FOUNDATION_EXPORT const unsigned char GIphyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GIphy/PublicHeader.h>
+
+
